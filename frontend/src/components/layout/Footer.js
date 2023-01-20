0a88b4aa92e99e57913f2667a43617522b55593b
Frederico Nogueira <fredericonogueira01@outlook.com>
2023-01-20 11:49:58 -0300

    Inicialização dos components

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,5 +1,7 @@
+import styles from './Footer.module.css'
+
 function Footer() {
-  return <footer>
+  return <footer className={styles.footer}>
     <p>Frederico Nogueira | <b>Get A Pet</b> &copy; 2023</p>
   </footer>
 }
